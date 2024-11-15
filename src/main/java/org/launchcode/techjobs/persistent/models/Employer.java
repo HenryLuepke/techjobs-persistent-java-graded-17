0dfa86d25ef987500730eb59c4528f15e22f37e1
HenryLuepke <henryluepke4@gmail.com>
2024-11-15 13:25:20 -0600

    Task 2 tests passing except sql query test

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location cannot be blank")
+    @Size(max = 255, message = "Location is too long")
+    private String location;
+
+    public Employer() {}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public  String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Description cannot be blank")
+    @Size(max = 255, message = "Description is too long")
+    private String description;
+
+    public Skill() {}
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
